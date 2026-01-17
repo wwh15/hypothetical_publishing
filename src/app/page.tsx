3f115ad4527e6d22da1cc1eca4310c11dae5f85a
natiw04 <natnael.worku@duke.edu>
2026-01-17 17:25:50 -0500

    checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
    <div>
-<<<<<<< HEAD
-      Hello DUKE
-=======
-      Hello Duke
->>>>>>> main
+      Hello DUKE University!
    </div>
   );
 }
