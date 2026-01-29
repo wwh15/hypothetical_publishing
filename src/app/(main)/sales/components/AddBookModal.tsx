d1bf3cb4119750d772f4023fe38ac0ab29758813
Samuel Obeng <sko14@duke.edu>
2026-01-28 21:41:37 -0500

    [Feature] auto-populate preview panel with book after it is created

--- src/app/(main)/sales/components/AddBookModal.tsx
+++ src/app/(main)/sales/components/AddBookModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -7,13 +9,19 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import BookForm from "../../books/components/BookForm";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AddBookModal() {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       {/* DialogTrigger wraps the button that opens the modal */}
       <DialogTrigger asChild>
-        <Button size="sm" variant="outline">Add Book</Button>
+        <Button size="sm" variant="outline">
+          Add Book
+        </Button>
       </DialogTrigger>
 
       {/* DialogContent contains everything inside the modal */}
@@ -22,7 +30,17 @@ export default function AddBookModal() {
           <DialogTitle>Add Book</DialogTitle>
         </DialogHeader>
 
-        <BookForm/>
+        <BookForm
+        mode="create"
+          inModal={true}
+          onModalSuccess={() => {
+            setOpen(false);
+            router.refresh()
+          }}
+          onModalCancel={() => {
+            setOpen(false);
+          }}
+        />
       </DialogContent>
     </Dialog>
   );
