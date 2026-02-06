cede915c96697a33ddd29fc2b5fccd2c7d935278
Aaron Bullock <ajbull426@gmail.com>
2026-02-06 12:46:30 -0500

    Fixed Sales Table to sort by date correctly

--- src/lib/data/records.ts
+++ src/lib/data/records.ts
@@ -2,7 +2,7 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "../prisma";
 
-export interface SaleListItem {
+export interface SaleListItem { 
   id: number;
   bookId: number;
   title: string;
